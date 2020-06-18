2a7927f4527978ad2afa14b084f66074e1ec1806
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-06-18 14:29:26 -0500

    javaScript exercise

---
+++ js/external.js
@@ -0,0 +1,42 @@
+console.log("Hello from external JavaScript.");
+//#1
+alert("Welcome to my Website!");
+
+// #2
+let answer = prompt("What is your favorite color?");
+alert("Great, " + answer + " is my favorite color too!");
+
+// #3
+let littleMermaidDays = prompt("How many days did you rent Little Mermaid?");
+let brotherBearDays = prompt('How many days did you rent Brother Bear?');
+let herculesDays = prompt("How many days did you rent Hercules?");
+let charges = 3*littleMermaidDays + 3*brotherBearDays + 3*herculesDays;
+alert("You owe $"+ charges + " for rental charges ($3 per movie per day').");
+
+const googleRate = 400;
+const amazonRate = 380;
+const facebookRate = 350;
+let googleHours = prompt("How many hours did you work for Google?");
+let amazonHours = prompt("How many hours did you work for Amazon?");
+let facebookHours = prompt("How many hours did you work for Facebook?");
+let totalPay = googleRate*googleHours + amazonRate*amazonHours + facebookRate*facebookHours;
+alert("You made $"+totalPay+ " this week.");
+
+let isClassOpen = confirm("Is there room in the class?");
+let isScheduleOpen = confirm("Does this class work with your schedule?");
+alert("You may register: " + (isClassOpen && isScheduleOpen));
+
+let numberItemsPurchased = parseInt(prompt("How many items did you purchase?"));
+let isOfferExpired = confirm("Press OK if the offer is not expired.");
+let isPremiumMember = confirm("Press OK if you are a premium member");
+// isOfferExpired && (numberItemsPurchased > 2 || isPremiumMember);
+alert("Offer Applied: " + (isOfferExpired && (numberItemsPurchased > 2 || isPremiumMember)));
+
+
+
+
+
+
+
+
+
